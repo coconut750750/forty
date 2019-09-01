767cacf0037d74c106c29d818e84839d6ca4ea4a
Brandon Wang <sourbrandon@gmail.com>
2019-09-01 13:31:00 -0500

    added player list component

---
+++ client/src/components/PlayerList.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './PlayerList.css';
+
+class PlayerList extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Players</p>
+        <div className="d-flex justify-content-center">
+          {this.props.players.map( player => (
+            <div class="badge badge-secondary badge-dark m-3">{ player.name }</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PlayerList;
