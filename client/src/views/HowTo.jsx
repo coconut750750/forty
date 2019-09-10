07e8b0e4b8fd4b41b1821529326a2fc30625cbf0
Brandon Wang <sourbrandon@gmail.com>
2019-09-10 10:38:44 -0500

    add how to page

---
+++ client/src/views/HowTo.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class HowTo extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>HowTo</p>
+
+      <button type="button" className="btn btn-light" onClick={this.props.goBack}>Back</button>
+
+      </div>
+    );
+  }
+}
+
+export default HowTo;
